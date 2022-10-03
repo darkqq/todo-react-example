fd0192dc473087ce3a803dc322d34633e3c3b3e7
darkqq <eugenebsuir@gmail.com>
2022-10-04 01:24:28 +0300

    init example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from "./components/todoItem/TodoItem";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([
+        {
+            text: 'text',
+            date: new Date().toString().slice(0, 10),
+            checked: true,
+            id: new Date().getTime().toString()
+        },
+        {
+            text: 'text',
+            date: new Date().toString().slice(0, 10),
+            checked: false,
+            id: new Date().getTime().toString() + '10'
+        }
+    ]);
+
+    function removeItem(id) {
+        setItems(items.filter(item => item.id !== id));
+    }
+
+    return (
+        <div>
+            {items.map(item => {
+                return <TodoItem
+                    handleClickRemove={(id) => removeItem(id)}
+                    key={item.id}
+                    item={item}
+                />
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/todoItem/TodoItem.js
@@ -0,0 +1,29 @@
+import './TodoItem.css'
+import {useState} from "react";
+
+function TodoItem(props) {
+    let [checked, isChecked] = useState(!!props.item.checked);
+
+    return (
+        <div className="todo-item">
+            <div className="todo-item__checkbox">
+                <input
+                    onChange={() => isChecked(!checked)}
+                    type={'checkbox'} checked={checked}/>
+            </div>
+            <div className="todo-item__text">
+                <input
+                    type={'text'} style={checked ? {textDecoration: 'line-through'} : {}}
+                    value={'Text'}
+                    disabled={true}/>
+            </div>
+            <div className="todo-item__actions">
+                <button onClick={() => props.handleClickRemove(props.item.id)}>X</button>
+                <input
+                    type={'text'}
+                    value={new Date().toString().slice(0, 10)}/>
+            </div>
+        </div>)
+}
+
+export default TodoItem;
